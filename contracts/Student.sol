8581099ada907f38f201cac47482040bdffe17df
Saheel Wagh <saheelwagh@Gmail.com>
2019-03-30 10:34:53 +0530

    Student can now addd a project

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.5.0;
+
 contract Student {
     string name;
+    uint public projectCount = 0;
+    uint publicprojectCount = 0;
     struct Project{
+        uint task;
         string name;
         bool done;
+    }
+    mapping(uint => Project ) public projects;
+    function addProject(string memory _project) public{ 
+        projectCount++;
+        projects[projectCount] = Project(projectCount,_project,false) ;
     }
 }
